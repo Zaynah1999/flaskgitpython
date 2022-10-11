b7cc861fda1e2ae38cf01210537b912f3cf002fe
Zaynah Ahmed <azaynah295@gmail.com>
2022-10-12 00:43:59 +0100

    consolidate pokemon datat endpoints into to simpler function

--- app.py
+++ app.py
@@ -10,32 +10,23 @@ def pokemon_list():
     return "Bulbasaur, charmander, pikachu, eevee, diglett"
 
 
-@app.get("/bulbasaur") # write after server link slash for each bit you want
-def bulbasaur_data():
-    return "This is bulbasaur, a generation 1 pokemon who looks like a tiny dinosaur"
+pokemon_creatures = {
+    "bulbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole",
+}
 
 
-@app.get("/charmander")
-def charmander_data():
-    return "This is charmander, a generation 1 pokemon who looks like a tiny reptile"
-
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "This is pikachu, a generation 1 pokemon who looks like a tiny rodent"
-
-
-@app.get("/eevee")
-def eevee_data():
-    return "This is eevee, a generation 1 pokemon who looks like a tiny fox"
-
-
-@app.get("/diglett")
-def diglett_data():
-    return "This is diglett, a generation 1 pokemon who looks like a tiny mole"
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
 
 
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),
             port=int(os.getenv('PORT', 4445)))
+
