b8451ce6b7b95af9b73b9b67e335a046aebb0599
Zaynah Ahmed <azaynah295@gmail.com>
2022-10-12 01:10:43 +0100

    Extract request logic to helpers.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from helpers import get_pokemon_by_name
 import os
-import requests
 
 app = Flask(__name__)
 
@@ -26,6 +26,29 @@ app = Flask(__name__)
 
 
 
+# @app.get("/")
+# def pokemon_list():
+#     return "Bulbasaur, charmander, pikachu, eevee, diglett"
+#
+#
+# @app.get("/<pokemon_name>")
+# def pokemon_data(pokemon_name):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+#     pokemon = response.json()
+#     return f"This is {pokemon['name']}.\n" \
+#            f"Height: {pokemon['height']}.\n" \
+#            f"Weight: {pokemon['weight']}.\n" \
+#            f"Base experience: {pokemon['base_experience']}.\n" \
+#            f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
+#
+#
+# if __name__ == "__main__":
+#     app.run(host=os.getenv('IP', '0.0.0.0'),
+#             port=int(os.getenv('PORT', 4445)))
+
+
+
+
 @app.get("/")
 def pokemon_list():
     return "Bulbasaur, charmander, pikachu, eevee, diglett"
@@ -33,8 +56,7 @@ def pokemon_list():
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
-    pokemon = response.json()
+    pokemon = get_pokemon_by_name(pokemon_name)
     return f"This is {pokemon['name']}.\n" \
            f"Height: {pokemon['height']}.\n" \
            f"Weight: {pokemon['weight']}.\n" \
@@ -45,4 +67,3 @@ def pokemon_data(pokemon_name):
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),
             port=int(os.getenv('PORT', 4445)))
-
