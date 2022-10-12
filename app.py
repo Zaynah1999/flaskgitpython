c20df13a7324ff5e1ba3aa79c4ba45a08c8a1c6d
Zaynah Ahmed <azaynah295@gmail.com>
2022-10-12 01:21:58 +0100

    generate random pokemon

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from helpers import get_pokemon_by_name
 import os
+from helpers import get_pokemon_by_name, get_random_pokemon_list
 
 app = Flask(__name__)
 
@@ -49,21 +49,43 @@ app = Flask(__name__)
 
 
 
+# @app.get("/")
+# def pokemon_list():
+#     return "Bulbasaur, charmander, pikachu, eevee, diglett"
+#
+#
+# @app.get("/<pokemon_name>")
+# def pokemon_data(pokemon_name):
+#     pokemon = get_pokemon_by_name(pokemon_name)
+#     return f"This is {pokemon['name']}.\n" \
+#            f"Height: {pokemon['height']}.\n" \
+#            f"Weight: {pokemon['weight']}.\n" \
+#            f"Base experience: {pokemon['base_experience']}.\n" \
+#            f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
+#
+# if __name__ == "__main__":
+#     app.run(host=os.getenv('IP', '0.0.0.0'),
+#             port=int(os.getenv('PORT', 4445)))
+
+
+
+
+
 @app.get("/")
 def pokemon_list():
-    return "Bulbasaur, charmander, pikachu, eevee, diglett"
+    return ', '.join([pokemon['name'] for pokemon in get_random_pokemon_list()]).title()
 
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
     pokemon = get_pokemon_by_name(pokemon_name)
-    return f"This is {pokemon['name']}.\n" \
+    return f"This is {pokemon['name'].capitalize()}.\n" \
            f"Height: {pokemon['height']}.\n" \
            f"Weight: {pokemon['weight']}.\n" \
            f"Base experience: {pokemon['base_experience']}.\n" \
            f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
-
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),
             port=int(os.getenv('PORT', 4445)))
+
