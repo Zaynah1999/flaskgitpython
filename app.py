3bfe0cb1aa4674e4d354ac221962dda171cd9eaa
Zaynah Ahmed <azaynah295@gmail.com>
2022-10-12 01:01:00 +0100

    refactor pokeomon data endpoint to use external api

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
 import os
-
+import requests
 
 app = Flask(__name__)
+
+
+# @app.get("/")
+# def pokemon_list():
+#     return "Bulbasaur, charmander, pikachu, eevee, diglett"
+#
+#
+# pokemon_creatures = {
+#     "bulbasaur": "dinosaur",
+#     "charmander": "reptile",
+#     "pikachu": "rodent",
+#     "eevee": "fox",
+#     "diglett": "mole",
+# }
+#
+#
+# @app.get("/<pokemon_name>")
+# def pokemon_data(pokemon_name):
+#     creature = pokemon_creatures.get(pokemon_name)
+#     return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
+
 
 
 @app.get("/")
@@ -10,20 +31,15 @@ def pokemon_list():
     return "Bulbasaur, charmander, pikachu, eevee, diglett"
 
 
-pokemon_creatures = {
-    "bulbasaur": "dinosaur",
-    "charmander": "reptile",
-    "pikachu": "rodent",
-    "eevee": "fox",
-    "diglett": "mole",
-}
-
-
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
-    creature = pokemon_creatures.get(pokemon_name)
-    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
-
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+    pokemon = response.json()
+    return f"This is {pokemon['name']}.\n" \
+           f"Height: {pokemon['height']}.\n" \
+           f"Weight: {pokemon['weight']}.\n" \
+           f"Base experience: {pokemon['base_experience']}.\n" \
+           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
 
 if __name__ == "__main__":
